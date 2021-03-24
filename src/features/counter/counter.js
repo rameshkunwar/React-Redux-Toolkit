ff93a1b1116499044b53e32cc416fe073c851de2
Ramesh Kunwar <rameshkunwar@hotmail.com>
2021-03-24 16:18:09 +0100

    first commit

---
+++ src/features/counter/counter.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, incrementByAmount} from './counterSlice'
+import {Button} from 'react-bootstrap'
+
+export function Counter(){
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
+
+    return (
+        <div>
+      <div>
+        <Button
+          className="btn btn-primary"
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+        className="btn btn-secondary"
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
+    </div>
+    )
+}
